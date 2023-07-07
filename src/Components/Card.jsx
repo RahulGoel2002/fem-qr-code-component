b672e3cb7295dcce86a30513c5792c872d34546c
RahulGoel2002 <rgoel1608@gmail.com>
2023-07-08 03:09:54 +0530

    change content

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,8 +3,8 @@ export default function Card() {
     return <div className="card py-3 rounded-4">
     <img src={image} className="card-img-top rounded" alt="..." />
     <div className="card-body">
-      <h5 className="card-title text-center mb-4 text-wrap"><div>Improve your front-end</div><div>skills by building projects</div></h5>
-      <div className="card-text text-center text-wrap"><div>Scan the QR code to visit Frontend</div><div>Mentor and take your coding skills to</div><div>the next level</div></div>
+      <h5 className="card-title text-center mb-4 text-wrap"><div>Meet someone with</div><div>Exceptional Frontend skills</div></h5>
+      <div className="card-text text-center text-wrap"><div>Hi! I am Rahul Goel</div><div>Scan the qr to visit my</div><div>portfolio</div></div>
       {/* <a href="/" className="btn btn-primary">Go somewhere</a> */}
     </div>
   </div>
